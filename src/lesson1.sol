7eac456e0fc44ab5f86a37fdfc2a8fcc0a1f06d8
pero4l <lilpero03@gmail.com>
2026-03-28 23:43:10 +0100

    lesson 1

---
+++ src/lesson1.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Counter {
+
+    uint256 public count;
+
+    function increment() public {
+        count += 1;
+    }
+
+    function decrement() public {
+        require(count > 0, "Cannot go below zero");
+        count -= 1;
+    }
+}
